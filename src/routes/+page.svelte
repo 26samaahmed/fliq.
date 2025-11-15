1dc70cb5ecb6bdafcbf43d53c74b1abf7cb2bb5f
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-11-15 15:20:35 -0800

    start base design for landing page
    Fonts don't work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script></script>
+
+<main class="bg-[#333745] min-h-screen p-6 text-center">
+  <div>
+    <h1 class="font-aldrich text-4xl text-white">Welcome!</h1>
+
+    <p class="font-arimo text-xl text-white">
+      Create and customize photo memories with your friends...
+    </p>
+
+    <button class="bg-[#EFF5DC] text-[#333745] p-2 rounded-full">
+      Start Taking Pictures
+    </button>
+  </div>
+</main>
+
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ['./src/**/*.{svelte,js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        aclonica: ['Aclonica', 'sans-serif'],
+        aldrich: ['Aldrich', 'sans-serif'],
+        arimo: ['Arimo', 'sans-serif']
+      }
+    }
+  }
+};
